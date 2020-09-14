c6f397aba7d2b5d621f07dcc500550bc32900d96
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-14 14:24:07 -0300

    adicionando componente contato

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -6,10 +6,12 @@ import{ MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,

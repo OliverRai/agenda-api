66b44cb62f09b80b5d94b78dbf5f6665e7a511b3
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-16 16:44:36 -0300

    upload de fotos com angular

--- agenda-app/src/app/contato/contato.component.ts
+++ agenda-app/src/app/contato/contato.component.ts
@@ -13,7 +13,7 @@ export class ContatoComponent implements OnInit {
 
   formulario: FormGroup;
   contatos: Contato [] = [];
-  colunas = ['id','nome','email','favorito'];
+  colunas = ['foto','id','nome','email','favorito'];
 
   constructor(
     private service: ContatoService,
@@ -51,5 +51,15 @@ export class ContatoComponent implements OnInit {
       let lista: Contato[] = [...this.contatos, response];
       this.contatos = lista;
     })
+  }
+
+  uploadFoto(event, contato){
+    const files = event.target.files;
+    if(files){
+      const foto = files[0];
+      const formData: FormData = new FormData;
+      formData.append("foto", foto);
+      this.service.upload(contato, formData).subscribe(reponse => this.listarContatos());
+    }
   }
 }
